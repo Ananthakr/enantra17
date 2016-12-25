b0d70c646aebbe8562c372f8944268f677aa06df
Md Faizaan <aulisius7@gmail.com>
2016-12-25 14:42:54 +0530

    changes #2

---
+++ js/enantra.mob.js
@@ -0,0 +1,20 @@
+function openD (evt, dName) {
+  var i, tabcontent, tablinks
+  tabcontent = document.getElementsByClassName('tabcontent')
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = 'none'
+  }
+  tablinks = document.getElementsByClassName('tablinks')
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(' active', '')
+  }
+  document.getElementById(dName).style.display = 'block'
+  evt.currentTarget.className += ' active'
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById('defaultOpen').click()
+
+document.getElementById('btn').addEventListener('click', function () {
+  document.getElementById('bar').classList.toggle('open')
+})
